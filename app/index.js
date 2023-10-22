4a49e14bf375301133fc0fc11cd2911bc81f3c83
dimitar.chardakliev <dimitar.chardakliev@scalefocus.com>
2023-10-22 09:21:34 +0300

    finally managed to run the application

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>
+                лао
+            </Text>
+        </View>
+    )
+}
+
+export default Home;
